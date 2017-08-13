090ec6fa5620ec1b891b8591ed447644c4eb0d35
ghoshrintesh <97rintesh.rg@gmail.com>
2017-08-13 20:45:30 +0530

    Add files via upload

---
+++ Assignment 1/CS-393-03.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void tower(char,char,char,int);
+int main()
+{
+    int n;
+    printf("Enter n\n");
+    scanf("%d",&n);
+    tower('A','C','B',n);
+}
+void tower(char s,char d,char m.int n)
+{
+    if(n==1)
+        printf("%c -> %c\n",s,d);
+    else
+        tower(s,m,d,n-1);
+        tower(s,d,m,1);
+        tower(m,d,s,n-1);
+}
