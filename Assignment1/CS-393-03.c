3e75c9455c9948f3ceb930f18d8028400aeb0049
Aurghyadip <adkundu@gmail.com>
2017-08-14 11:09:17 +0530

    removed mistake in code

--- Assignment1/CS-393-03.c
+++ Assignment1/CS-393-03.c
@@ -7,12 +7,14 @@ int main()
     scanf("%d",&n);
     tower('A','C','B',n);
 }
-void tower(char s,char d,char m.int n)
+void tower(char s, char d, char m, int n)
 {
     if(n==1)
         printf("%c -> %c\n",s,d);
     else
+	{
         tower(s,m,d,n-1);
         tower(s,d,m,1);
         tower(m,d,s,n-1);
+	}
 }
